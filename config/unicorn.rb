2182d3ac591c79cd09e571802c91e89459afa461
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-12-31 10:19:27 +0900

    Configure Unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,34 @@
+app_path = ENV['APP_PATH']
+
+worker_processes 2
+working_directory "#{app_path}/"
+
+preload_app true
+
+timeout 30
+
+listen '/tmp/unicorn_stay.sock', backlog: 64
+
+pid '/tmp/unicorn_stay.pid'
+
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+    if old_pid != server.pid
+      begin
+        sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+        Process.kill(sig, File.read(old_pid).to_i)
+      rescue Errno::ENOENT, Errno::ESRCH
+      end
+    end
+
+    sleep 1
+  end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
