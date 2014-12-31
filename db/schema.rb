ac13281042740e5109073d5285fcb8adb0bff700
Ryota Kameoka <kameoka.ryota@gmail.com>
2014-12-31 09:40:39 +0900

    Add Skill model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141230234250) do
+ActiveRecord::Schema.define(version: 20141231003903) do
 
   create_table "awards", force: :cascade do |t|
     t.string   "award",      limit: 255, null: false
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20141230234250) do
     t.string   "description", limit: 255, null: false
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
