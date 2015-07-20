0e6a0842158772c1427406b39226c927757507e7
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-07-20 13:58:36 +0900

    Update profile section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,12 @@
 class PagesController < ApplicationController
   def index
-    @products = Product.all
-
     today = Date.today.strftime('%Y%m%d').to_i
     birthday = Date.new(1993, 6, 7).strftime('%Y%m%d').to_i
-    age = (today - birthday) / 10_000
-
-    @profiles = {
-      birthday: "1993.06.07 (age #{age})",
-      location: 'ichijoji, sakyo, kyoto',
-      hometown: 'kishiwada, osaka',
-      education: 'kyoto university (faculty of economics)'
-    }
+    @age = (today - birthday) / 10_000
 
     @skills = Skill.all
     @awards = Award.all.order(:date)
     @links = Link.all
+    @products = Product.all
   end
 end
