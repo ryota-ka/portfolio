fee1ae1c028a1d0054861c8e0b027f2a11530715
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-08-15 04:41:12 +0900

    Introduce customized Bootstrap

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function() {
-  $('nav').find('a').on('click', function(e) {
+  $('#status-bar').find('a').on('click', function(e) {
     e.preventDefault();
     var target = $(this).attr('href');
     $('html, body').animate({ scrollTop: $(target).offset().top }, 500);
