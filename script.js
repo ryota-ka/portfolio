4374c6499a1612fb5344e3126729a105a37b4496
Ryota Kameoka <kameoka.ryota@gmail.com>
2015-08-15 06:09:05 +0900

    Tune up scrolling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function() {
   $('#status-bar').find('a').on('click', function(e) {
     e.preventDefault();
     var target = $(this).attr('href');
-    $('html, body').animate({ scrollTop: $(target).offset().top }, 500);
+    $('html, body').animate({ scrollTop: $(target).offset().top + 10 }, 500);
   });
 
   var pad = function(n) {
