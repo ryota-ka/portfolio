dde50edb4c0009fca6653842af9cf6c6d830a797
Ryota Kameoka <kameoka.ryota@gmail.com>
2018-01-21 02:50:05 +0900

    Remove navigation

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,6 @@
 import './GitHub';
 import './HatenaBlog';
 import './Lastfm';
-import * as $ from 'jquery';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const photo = document.getElementById('photo');
@@ -13,15 +12,4 @@ document.addEventListener('DOMContentLoaded', async () => {
         if (photo) photo.style.filter = `blur(${offset / 250}px)`;
         if (overlay) overlay.style.opacity = String(offset / 7500);
     })
-
-    const anchors = document.querySelectorAll('nav a') as NodeListOf<HTMLAnchorElement>;
-
-    Array.from(anchors).forEach(a => {
-        const target = document.getElementById(a.dataset.target || '');
-        if (target === null) return;
-
-        const scrollTop = target.offsetTop + 1;
-
-        $('html, body').animate({ scrollTop }, 500);
-    });
 });
